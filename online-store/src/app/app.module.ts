30c098f7b33d255652a77fd494a765e025623779
HERNAN PINOS <33234911+hernanpinos15@users.noreply.github.com>
2020-04-22 21:59:17 -0500

    Creando el detalle de cada producto en el proyecto

--- online-store/src/app/app.module.ts
+++ online-store/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HeaderComponent,
     FooterComponent,
     BannerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
--- online-store/src/app/products/products.component.ts
+++ online-store/src/app/products/products.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Product } from './../product.model';
 
 @Component({
@@ -10,51 +9,6 @@ import { Product } from './../product.model';
 export class ProductsComponent implements OnInit {
 
   constructor() { }
-
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/1.jpg',
-      title: 'Moto 1',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '2',
-      image: 'assets/images/2.jpg',
-      title: 'Moto 2',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '3',
-      image: 'assets/images/3.jpg',
-      title: 'Moto 3',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '4',
-      image: 'assets/images/4.jpg',
-      title: 'Moto 4',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '5',
-      image: 'assets/images/5.jpg',
-      title: 'Moto 5',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '6',
-      image: 'assets/images/6.jpg',
-      title: 'Moto 6',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    }
-  ];
 
   ngOnInit(): void {
   }
