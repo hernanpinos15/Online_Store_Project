174dae59705b238368e6c33685069861971f1790
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-02 16:59:51 -0500

    Creando el conetido para el footer

--- online-store/src/app/material/material.module.ts
+++ online-store/src/app/material/material.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
--- online-store/src/app/shared/shared.module.ts
+++ online-store/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from './../material/material.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
