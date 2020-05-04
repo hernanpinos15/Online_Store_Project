178ab079781e393795c01d887f4e8104f77e5261
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-04 16:31:01 -0500

    Se borro los datos de los productos

--- online-store/src/app/product/components/products/products.component.ts
+++ online-store/src/app/product/components/products/products.component.ts
@@ -11,53 +11,6 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
 
-  /*
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/1.jpg',
-      title: 'Moto 1',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '2',
-      image: 'assets/images/2.jpg',
-      title: 'Moto 2',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '3',
-      image: 'assets/images/3.jpg',
-      title: 'Moto 3',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '4',
-      image: 'assets/images/4.jpg',
-      title: 'Moto 4',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '5',
-      image: 'assets/images/5.jpg',
-      title: 'Moto 5',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    },
-    {
-      id: '6',
-      image: 'assets/images/6.jpg',
-      title: 'Moto 6',
-      price: 1000,
-      description: 'bla ble bli blo blu'
-    }
-  ];
-  */
-
   constructor(
     private productsService: ProductsService
   ) { }
