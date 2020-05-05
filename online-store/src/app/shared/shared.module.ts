6b4aa8a9a7375be0f0d47877da3d3a72b97d2dd4
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-04 19:55:35 -0500

    Ambientes de Angulaar crear varios ambientes

--- online-store/src/app/shared/shared.module.ts
+++ online-store/src/app/shared/shared.module.ts
@@ -7,6 +7,9 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+
+
 import { MaterialModule } from './../material/material.module';
 
 @NgModule({
@@ -14,7 +17,8 @@ import { MaterialModule } from './../material/material.module';
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent
   ],
   exports: [
     ExponentialPipe,
