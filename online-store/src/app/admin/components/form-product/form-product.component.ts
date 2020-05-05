2556b0053ef1697699f0e765d3c64ab5f1cfb7b8
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-05 18:10:39 -0500

    Creando el formulario de productos

---
+++ online-store/src/app/admin/components/form-product/form-product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.scss']
+})
+export class FormProductComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.builForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  private builForm(){
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      image: [''],
+      description: ['', [Validators.required]],
+    });
+  }
+}
